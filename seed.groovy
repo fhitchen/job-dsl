3efaf7cac71dbbd9773f5c515b988b7a8f70d6e3
Greg Lonnon <greg.lonnon@gmail.com>
2016-06-16 12:08:40 -0600

    Boise Devops 2016

--- seed.groovy
+++ seed.groovy
@@ -1,36 +1,80 @@
-String project= 'boise_devops_2016'
+#!groovy
 
-String gitUrl = 'git@bitbucket.org:${project}'
-String org = 'devteam'
-
+// job-dsl langauge
+String team= 'boise_devops_2016'
+String gitUrl = "https://bitbucket.org/$team"
+String cronSchedule = 'H/2 * * * *'
 services = ['hello', 'goodbye']
 branches = ['master', 'qa', 'staging', 'prod']
 
+folder "$team"
+
 services.each {
   service = it
+  currentFolder = "$team/$service"
+  folder currentFolder
 
   branches.each {
     branch = it
 
-    String currentFolder = '$project/$service/$branch'
-
-    folder currentFolder
-
-    pipelineJob("$currentFolder/$branch") {
-
+    pipelineJob ("$currentFolder/$branch") {
+      description("do not hand edit, built by seed.groovy")
       triggers {
-          scm('H/5 * * * *')
+          scm cronSchedule
+      }
+      scm {
+        git {
+          remote {
+            url "$gitUrl/$service"
+            branch "$branch"
+          }
+        }
       }
       definition {
-
-        cpsScm {
-            scm {
-                git('$gitUrl/$service')
+  	     cpsScm {
+    	      scm {
+    		        git {
+      		          remote {
+        		            url "$gitUrl/$service"
+        		            branch "$branch"
+      		          }
+    	          }
             }
-            scriptPath('Jenkinsfile-$branch.groovy')
+            scriptPath "Jenkinsfile_$branch"
         }
       }
     }
   }
+
+  // a benchtest for each repo...
+  multibranchPipelineJob ("$team/$service/benchtest") {
+    description("Benchtest pipelines. Push commit/branch/tag to git push <branchname>:refs/heads/benchtest/<branchname>, then the Jenkinsfile pipeline will execute" )
+    branchSources {
+        git {
+          remote "$gitUrl/$service"
+          includes "benchtest/**"
+        }
+    }
+    triggers {
+      cron cronSchedule
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep 20
+        }
+    }
+  }
+  // a release area for each repo...
+  multibranchPipelineJob("$team/$service/releases") {
+    description "Releease pipelines. Push commit/branch/tag to git push <branchname>:refs/heads/releases/<branchname>, then the Jenkinsfile pipeline will execute"
+    branchSources {
+        git {
+          remote "$gitUrl/$service"
+          includes "releases/**"
+        }
+    }
+    triggers {
+      cron cronSchedule
+    }
+  }
 }
-~
---
+++ tests/hello_job_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+echo "hello job testing"
+
+# change this REPO to your location
+REPO=git@bitbucket.org:boise_devops_2016/hello.git
+rm -rf tmp
+mkdir -p tmp
+cd tmp
+git clone $REPO test
+cd test
+TIMESTAMP=$(date +%s)
+BRANCHES=("qa" "staging" "prod" "benchtest/br_$TIMESTAMP" "releases/br_$TIMESTAMP" "benchtest/br_1" "releases/br_1")
+for branch in "${BRANCHES[@]}"
+do
+  git checkout origin/master
+  echo "echo hello from branch= $branch" > hello.sh
+  git commit -a -m "changes to $branch"
+  git push -f origin HEAD:refs/heads/$branch
+  echo $branch
+done
+
+rm -rf tmp
